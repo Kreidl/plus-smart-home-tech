6dfc7bc2dcfb1afe6b858e6a8d90f67174022c6a
Карина <karinkademchenko@yandex.ru>
2026-03-06 20:45:35 +0300

    Corrections in sensor handlers

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/service/handler/sensor/MotionEventHandler.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/service/handler/sensor/MotionEventHandler.java
@@ -1,10 +1,10 @@
 package ru.yandex.practicum.telemetry.collector.service.handler.sensor;
 
 import org.springframework.stereotype.Component;
+import ru.yandex.practicum.grpc.telemetry.event.MotionSensorProto;
+import ru.yandex.practicum.grpc.telemetry.event.SensorEventProto;
 import ru.yandex.practicum.kafka.telemetry.event.MotionSensorAvro;
 import ru.yandex.practicum.telemetry.collector.configuration.KafkaProducerConfig;
-import ru.yandex.practicum.telemetry.collector.model.sensor.MotionSensorEvent;
-import ru.yandex.practicum.telemetry.collector.model.sensor.SensorEvent;
 import ru.yandex.practicum.telemetry.collector.model.sensor.SensorEventType;
 
 @Component(value = "MOTION_SENSOR")
@@ -20,12 +20,17 @@ public class MotionEventHandler extends BaseSensorEventHandler<MotionSensorAvro>
     }
 
     @Override
-    public MotionSensorAvro mapToAvro(SensorEvent sensorEvent) {
-        MotionSensorEvent motionSensorEvent = (MotionSensorEvent) sensorEvent;
+    public SensorEventProto.PayloadCase getMessageTypeProto() {
+        return SensorEventProto.PayloadCase.MOTION_SENSOR;
+    }
+
+    @Override
+    public MotionSensorAvro protoToAvro(SensorEventProto sensorEvent) {
+        MotionSensorProto motionSensorProto = sensorEvent.getMotionSensor();
         return MotionSensorAvro.newBuilder()
-                .setMotion(motionSensorEvent.getMotion())
-                .setLinkQuality(motionSensorEvent.getLinkQuality())
-                .setVoltage(motionSensorEvent.getVoltage())
+                .setMotion(motionSensorProto.getMotion())
+                .setLinkQuality(motionSensorProto.getLinkQuality())
+                .setVoltage(motionSensorProto.getVoltage())
                 .build();
     }
 }
