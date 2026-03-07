4fa096fe6e7ac63279fbc290b2714d91b2ca75e3
Карина <karinkademchenko@yandex.ru>
2026-03-08 02:21:59 +0300

    Edit HubEventHandler

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/service/handler/hub/HubEventHandler.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/service/handler/hub/HubEventHandler.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.telemetry.collector.service.handler.hub;
 
 import ru.yandex.practicum.grpc.telemetry.event.HubEventProto;
-import ru.yandex.practicum.telemetry.collector.model.hub.HubEvent;
 import ru.yandex.practicum.telemetry.collector.model.hub.HubEventType;
 
 public interface HubEventHandler {
