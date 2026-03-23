54b4800512bd720f28ca30c803402e60cacdb0f1
Карина <karinkademchenko@yandex.ru>
2026-03-23 12:55:23 +0300

    Add dto for module shopping-cart

---
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/cart/dto/ChangeProductQuantityRequest.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.cart.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
+public record ChangeProductQuantityRequest (@NotNull UUID productId, @NotNull  Long quantity) {}
