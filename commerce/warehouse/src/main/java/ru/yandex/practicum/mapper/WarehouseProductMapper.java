1ad8b726f0e38140cdcfcb57cc0c98c1f55b76ca
Карина <karinkademchenko@yandex.ru>
2026-03-23 15:48:56 +0300

    Add mapper in module warehouse

---
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/mapper/WarehouseProductMapper.java
@@ -0,0 +1,20 @@
+package ru.yandex.practicum.mapper;
+
+import lombok.experimental.UtilityClass;
+import ru.yandex.practicum.model.WarehouseProduct;
+import ru.yandex.practicum.warehouse.dto.NewProductInWarehouseRequest;
+
+@UtilityClass
+public class WarehouseProductMapper {
+    public static WarehouseProduct mapToEntity(NewProductInWarehouseRequest request) {
+        WarehouseProduct warehouseProduct = new WarehouseProduct();
+        warehouseProduct.setProductId(request.productId());
+        warehouseProduct.setWidth(request.dimensionDto().width());
+        warehouseProduct.setHeight(request.dimensionDto().height());
+        warehouseProduct.setDepth(request.dimensionDto().depth());
+        warehouseProduct.setWeight(request.weight());
+        warehouseProduct.setFragile(request.fragile());
+        warehouseProduct.setQuantity(0L);
+        return warehouseProduct;
+    }
+}
