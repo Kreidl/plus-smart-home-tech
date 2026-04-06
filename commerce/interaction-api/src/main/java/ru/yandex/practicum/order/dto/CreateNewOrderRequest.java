0ee7015d0e29a5230ad3f75a731cd9f3a48bd3e4
Карина <karinkademchenko@yandex.ru>
2026-04-06 19:22:34 +0300

    Add new dtos and enums

---
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/order/dto/CreateNewOrderRequest.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.order.dto;
+
+import jakarta.validation.constraints.NotNull;
+import ru.yandex.practicum.cart.dto.ShoppingCartDto;
+import ru.yandex.practicum.warehouse.dto.AddressDto;
+
+public record CreateNewOrderRequest(@NotNull ShoppingCartDto shoppingCartDto, @NotNull AddressDto addressDto) {}
